cc19c2fe519a6012237310fff39a32ffd71424bb
Gabriel Costa <woutogamer51@gmail.com>
2024-09-17 21:16:58 -0300

    iente_se/lib/screens/ranking_empresa_page.dart

--- ambiente_se/lib/screens/ranking_empresa_page.dart
+++ ambiente_se/lib/screens/ranking_empresa_page.dart
@@ -22,136 +22,165 @@ class RankingEmpresaPage extends StatelessWidget {
         backgroundColor: Colors.blue,
         leading: Icon(Icons.menu),
         title: Row(
-          mainAxisAlignment: MainAxisAlignment.center, // Centraliza o título na AppBar
+          mainAxisAlignment: MainAxisAlignment.end,
           children: [
             Icon(Icons.lightbulb, color: Colors.yellowAccent),
           ],
         ),
       ),
-      body: SingleChildScrollView(
-        child: Center( // Envolve o conteúdo principal em um widget Center
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center, // Centraliza verticalmente
-            crossAxisAlignment: CrossAxisAlignment.center, // Centraliza horizontalmente
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: TextField(
-                  decoration: InputDecoration(
-                    labelText: "Buscar por nome",
-                    border: OutlineInputBorder(),
-                    prefixIcon: Icon(Icons.search),
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          return SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // Search Field
+                Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      labelText: "Buscar por nome",
+                      border: OutlineInputBorder(),
+                      prefixIcon: Icon(Icons.search),
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(height: 20),
+                SizedBox(height: 10),
 
-              // Ranking General Section
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                child: Row(
+                // Ranking General Section
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: constraints.maxWidth * 0.04),
+                  child: Center(
+                    child: FittedBox(
+                      fit: BoxFit.scaleDown,
+                      child: Container(
+                        constraints: BoxConstraints(maxWidth: 600),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            CircleAvatar(
+                              radius: 50,
+                              backgroundImage: NetworkImage('https://via.placeholder.com/150'),
+                            ),
+                            SizedBox(width: 20),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text('Ranking Geral', style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+                                SizedBox(height: 4),
+                                Text('1ª Posição', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+                                SizedBox(height: 4),
+                                Text('Biopark', style: TextStyle(fontSize: 18)),
+                                Text('Ramo: Educacional', style: TextStyle(fontSize: 16, color: Colors.grey)),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20),
+
+                // Filters
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center, // Center the row contents
+                    children: [
+                      SizedBox(
+                        width: 150, // Fixed width for the dropdown
+                        child: DropdownButtonFormField<String>(
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            contentPadding: EdgeInsets.symmetric(vertical: 6.0, horizontal: 6.0), // Reduzido para ajustar o padding interno
+                          ),
+                          hint: Text("Filtrar por ramo", style: TextStyle(fontSize: 12)), // Reduzido o tamanho do texto
+                          items: ['Ramo 1', 'Ramo 2']
+                              .map((String value) {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(value, style: TextStyle(fontSize: 12)), // Reduzido o tamanho do texto
+                            );
+                          }).toList(),
+                          onChanged: (newValue) {},
+                        ),
+                      ),
+                      SizedBox(width: 10), // Increased space between dropdowns
+                      SizedBox(
+                        width: 150, // Fixed width for the dropdown
+                        child: DropdownButtonFormField<String>(
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            contentPadding: EdgeInsets.symmetric(vertical: 6.0, horizontal: 6.0), // Reduzido para ajustar o padding interno
+                          ),
+                          hint: Text("Filtrar por porte", style: TextStyle(fontSize: 12)), // Reduzido o tamanho do texto
+                          items: ['Porte 1', 'Porte 2']
+                              .map((String value) {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(value, style: TextStyle(fontSize: 12)), // Reduzido o tamanho do texto
+                            );
+                          }).toList(),
+                          onChanged: (newValue) {},
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 10),
+
+                // Ranking Table Section
+                Center(
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 10.0),
+                    child: SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: ConstrainedBox(
+                        constraints: BoxConstraints(minWidth: constraints.maxWidth - 40),
+                        child: DataTable(
+                          columnSpacing: 20,
+                          columns: const [
+                            DataColumn(label: Text('Posição', style: TextStyle(fontWeight: FontWeight.bold))),
+                            DataColumn(label: Text('Nome', style: TextStyle(fontWeight: FontWeight.bold))),
+                            DataColumn(label: Text('Ramo', style: TextStyle(fontWeight: FontWeight.bold))),
+                            DataColumn(label: Text('Cidade', style: TextStyle(fontWeight: FontWeight.bold))),
+                          ],
+                          rows: List<DataRow>.generate(
+                            10,
+                            (index) => DataRow(cells: [
+                              DataCell(Text('${index + 1}º')),
+                              DataCell(Text('Biopark')),
+                              DataCell(Text('Educacional')),
+                              DataCell(Text('Toledo')),
+                            ]),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 8),
+
+                // Pagination
+                Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    CircleAvatar(
-                      radius: 50, // Define the size of the circle
-                      backgroundImage: NetworkImage('https://via.placeholder.com/150'), // Placeholder image
+                    IconButton(
+                      icon: Icon(Icons.arrow_back),
+                      onPressed: () {},
                     ),
-                    SizedBox(width: 20),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text('Ranking Geral', style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
-                        Text('1ª Posição', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-                        Text('Biopark', style: TextStyle(fontSize: 18)),
-                        Text('Ramo: Educacional', style: TextStyle(fontSize: 16, color: Colors.grey)),
-                      ],
+                    Text('Página 1'),
+                    IconButton(
+                      icon: Icon(Icons.arrow_forward),
+                      onPressed: () {},
                     ),
                   ],
                 ),
-              ),
-              SizedBox(height: 20),
-
-              // Filters
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 15.0),
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: DropdownButtonFormField<String>(
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                        ),
-                        hint: Text("Filtrar por ramo"),
-                        items: ['Ramo 1', 'Ramo 2']
-                            .map((String value) {
-                          return DropdownMenuItem<String>(
-                            value: value,
-                            child: Text(value),
-                          );
-                        }).toList(),
-                        onChanged: (newValue) {},
-                      ),
-                    ),
-                    SizedBox(width: 6),
-                    Expanded(
-                      child: DropdownButtonFormField<String>(
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                        ),
-                        hint: Text("Filtrar por porte"),
-                        items: ['Porte 1', 'Porte 2']
-                            .map((String value) {
-                          return DropdownMenuItem<String>(
-                            value: value,
-                            child: Text(value),
-                          );
-                        }).toList(),
-                        onChanged: (newValue) {},
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(height: 15),
-
-              // Ranking Table Section
-              DataTable(
-                columns: const [
-                  DataColumn(label: Text('Posição')),
-                  DataColumn(label: Text('Nome')),
-                  DataColumn(label: Text('Ramo')),
-                  DataColumn(label: Text('Cidade')),
-                ],
-                rows: List<DataRow>.generate(
-                  10,
-                  (index) => DataRow(cells: [
-                    DataCell(Text('${index + 1}º')),
-                    DataCell(Text('Biopark')),
-                    DataCell(Text('Educacional')),
-                    DataCell(Text('Toledo')),
-                  ]),
-                ),
-              ),
-              SizedBox(height: 20),
-
-              // Pagination
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  IconButton(
-                    icon: Icon(Icons.arrow_back),
-                    onPressed: () {},
-                  ),
-                  Text('Página 1'),
-                  IconButton(
-                    icon: Icon(Icons.arrow_forward),
-                    onPressed: () {},
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ),
+              ],
+            ),
+          );
+        },
       ),
     );
   }
