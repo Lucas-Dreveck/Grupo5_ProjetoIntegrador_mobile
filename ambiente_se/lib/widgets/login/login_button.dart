4e7962d369cd744bd5043d60fd841994553e1d84
Daniel <danielhhartmann@gmail.com>
2024-10-19 14:53:43 -0300

    fix: login button working on web

--- ambiente_se/lib/widgets/login/login_button.dart
+++ ambiente_se/lib/widgets/login/login_button.dart
@@ -7,18 +7,16 @@ class LoginButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    double screenWidth = MediaQuery.of(context).size.width;
-    double fontSize = screenWidth * 0.05; // Adjust font size based on screen width
 
     return SizedBox(
       height: 50, // Increase the height of the button
-      width: screenWidth * 0.8, // Use 80% of the screen width
+      width: 300, // Use 80% of the screen width
       child: ElevatedButton(
         onPressed: onPressed,
         child: Text(
           'Login',
           style: TextStyle(
-            fontSize: fontSize,
+            fontSize: 16,
             fontWeight: FontWeight.w500,
             color: const Color.fromARGB(255, 245, 245, 245),
           ),
